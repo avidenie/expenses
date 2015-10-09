e8f5a34e1f86bf71cb79f6e55e8e87ea01ac40f3
Adrian Videnie <avidenie@gmail.com>
2015-10-09 21:38:46 +0300

    Add the design support library

--- app/src/main/java/ro/expectations/expenses/ui/accounts/AccountsActivity.java
+++ app/src/main/java/ro/expectations/expenses/ui/accounts/AccountsActivity.java
@@ -1,9 +1,13 @@
 package ro.expectations.expenses.ui.accounts;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import ro.expectations.expenses.R;
 
@@ -15,6 +19,17 @@ public class AccountsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accounts);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     @Override
