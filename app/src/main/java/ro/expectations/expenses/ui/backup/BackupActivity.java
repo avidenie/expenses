2f460bc7d9cf5447716036d9d5af8bd94b7cd350
Adrian Videnie <avidenie@gmail.com>
2015-11-03 23:11:38 +0200

    Add an alert dialog for importing Financisto backups

--- app/src/main/java/ro/expectations/expenses/ui/backup/BackupActivity.java
+++ app/src/main/java/ro/expectations/expenses/ui/backup/BackupActivity.java
@@ -1,10 +1,16 @@
 package ro.expectations.expenses.ui.backup;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewStub;
 
@@ -33,5 +39,41 @@ public class BackupActivity extends AppCompatActivity {
         ViewStub mainContent = (ViewStub) findViewById(R.id.main_content);
         mainContent.setLayoutResource(R.layout.content_backup);
         mainContent.inflate();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_backup, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_import_from_financisto) {
+            new ImportFromFinancistoDialogFragment().show(getSupportFragmentManager(), null);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public static class ImportFromFinancistoDialogFragment extends DialogFragment {
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+            builder.setTitle("AppCompatDialog");
+            builder.setMessage("Lorem ipsum dolor...");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                }
+            });
+            return builder.create();
+        }
     }
 }
