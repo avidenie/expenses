4fe23676c52f7e1ca985f8c54b1f32c8d77862a4
Adrian Videnie <avidenie@gmail.com>
2015-11-04 09:29:50 +0200

    Switch to support library fragments

--- app/src/main/java/ro/expectations/expenses/ui/backup/BackupFragment.java
+++ app/src/main/java/ro/expectations/expenses/ui/backup/BackupFragment.java
@@ -1,7 +1,7 @@
 package ro.expectations.expenses.ui.backup;
 
-import android.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
