c04050865cc2e3aa1b2d3b9c286d754b9e29d4d4
Adrian Videnie <avidenie@gmail.com>
2016-01-22 15:39:03 +0200

    Handle clicks and multiple selections on categories listing

--- app/src/main/java/ro/expectations/expenses/ui/categories/CategoriesAdapter.java
+++ app/src/main/java/ro/expectations/expenses/ui/categories/CategoriesAdapter.java
@@ -13,16 +13,17 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import ro.expectations.expenses.R;
+import ro.expectations.expenses.helper.ListHelper;
+import ro.expectations.expenses.widget.recyclerview.MultipleSelectionAdapter;
 
-public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.ViewHolder> {
+public class CategoriesAdapter extends MultipleSelectionAdapter<CategoriesAdapter.ViewHolder> {
 
     private Cursor mCursor;
     final private Context mContext;
-    final private OnClickListener mClickListener;
 
-    public CategoriesAdapter(Context context, OnClickListener clickListener) {
+    public CategoriesAdapter(Context context) {
         mContext = context;
-        mClickListener = clickListener;
+        setHasStableIds(true);
     }
 
     @Override
@@ -36,15 +37,19 @@ public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.Vi
     public void onBindViewHolder(ViewHolder holder, int position) {
         mCursor.moveToPosition(position);
 
-        // Set the icon background color.
-        GradientDrawable bgShape = (GradientDrawable) holder.mIconBackgroundView.getBackground();
+        // Set the row background
+        ListHelper.setItemBackground(mContext, holder.itemView, isItemSelected(position),
+                holder.mCategoryIconBackground, holder.mSelectedIconBackground);
+
+        // Set the icon background color
+        GradientDrawable bgShape = (GradientDrawable) holder.mCategoryIconBackground.getBackground();
         bgShape.setColor(0xFF000000 | ContextCompat.getColor(mContext, R.color.primary));
 
-        // Set the category name.
+        // Set the category name
         String name = mCursor.getString(CategoriesFragment.COLUMN_CATEGORY_NAME);
         holder.mNameView.setText(name);
 
-        // Show or hide the subcategories icon.
+        // Show or hide the subcategories icon
         long parentId = mCursor.getLong(CategoriesFragment.COLUMN_CATEGORY_PARENT_ID);
         if (parentId > 0 ) {
             holder.mSubcategoriesIcon.setVisibility(View.GONE);
@@ -66,6 +71,12 @@ public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.Vi
         return mCursor.getCount();
     }
 
+    @Override
+    public long getItemId(int position) {
+        mCursor.moveToPosition(position);
+        return mCursor.getLong(CategoriesFragment.COLUMN_CATEGORY_ID);
+    }
+
     public void swapCursor(Cursor newCursor) {
         mCursor = newCursor;
         notifyDataSetChanged();
@@ -75,33 +86,23 @@ public class CategoriesAdapter extends RecyclerView.Adapter<CategoriesAdapter.Vi
         return mCursor;
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        public final RelativeLayout mIconBackgroundView;
-        public final ImageView mIconView;
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public final RelativeLayout mCategoryIconBackground;
+        public final ImageView mCategoryIcon;
+        public final RelativeLayout mSelectedIconBackground;
+        public final ImageView mSelectedIcon;
         public final TextView mNameView;
         public final ImageView mSubcategoriesIcon;
 
         public ViewHolder(View view) {
             super(view);
-            mIconBackgroundView = (RelativeLayout) view.findViewById(R.id.category_icon_background);
-            mIconView = (ImageView) view.findViewById(R.id.category_icon);
+            mCategoryIconBackground = (RelativeLayout) view.findViewById(R.id.category_icon_background);
+            mCategoryIcon = (ImageView) view.findViewById(R.id.category_icon);
+            mSelectedIconBackground = (RelativeLayout) view.findViewById(R.id.selected_icon_background);
+            mSelectedIcon = (ImageView) view.findViewById(R.id.selected_icon);
             mNameView = (TextView) view.findViewById(R.id.category_name);
             mSubcategoriesIcon = (ImageView) view.findViewById(R.id.subcategories_icon);
-            view.setOnClickListener(this);
         }
-
-        @Override
-        public void onClick(View v) {
-            if (mClickListener != null) {
-                mCursor.moveToPosition(getAdapterPosition());
-                mClickListener.onClick(
-                        mCursor.getLong(CategoriesFragment.COLUMN_CATEGORY_ID),
-                        this);
-            }
-        }
-    }
-
-    public interface OnClickListener {
-        void onClick(long categoryId, ViewHolder vh);
     }
 }
--- app/src/main/java/ro/expectations/expenses/ui/categories/CategoriesFragment.java
+++ app/src/main/java/ro/expectations/expenses/ui/categories/CategoriesFragment.java
@@ -1,5 +1,6 @@
 package ro.expectations.expenses.ui.categories;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -7,16 +8,23 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ActionMode;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import ro.expectations.expenses.R;
 import ro.expectations.expenses.provider.ExpensesContract;
+import ro.expectations.expenses.ui.drawer.DrawerActivity;
 import ro.expectations.expenses.widget.recyclerview.DividerItemDecoration;
+import ro.expectations.expenses.widget.recyclerview.ItemClickHelper;
 
 public class CategoriesFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
 
@@ -34,12 +42,66 @@ public class CategoriesFragment extends Fragment implements LoaderManager.Loader
     private static final String ARG_PARENT_CATEGORY_ID = "parent_category_id";
 
     private long mParentCategoryId;
-    private RecyclerView mRecyclerView;
+    private CategoriesAdapter mAdapter;
     private TextView mEmptyView;
 
-    public CategoriesFragment() {
-        // Required empty public constructor
-    }
+    private ActionMode mActionMode;
+    private ActionMode.Callback mActionModeCallback = new ActionMode.Callback() {
+
+        @Override
+        public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+            MenuInflater inflater = mode.getMenuInflater();
+            inflater.inflate(R.menu.context_menu_categories, menu);
+            Activity activity = getActivity();
+            if (activity instanceof DrawerActivity) {
+                ((DrawerActivity) activity).lockNavigationDrawer();
+            }
+            return true;
+        }
+
+        @Override
+        public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+            int selectedCategories = mAdapter.getSelectedItemCount();
+            mode.setTitle(getResources().getQuantityString(
+                    R.plurals.selected_categories,
+                    selectedCategories,
+                    selectedCategories
+            ));
+            if (mAdapter.getSelectedItemCount() == 1) {
+                menu.findItem(R.id.action_edit_category).setVisible(true);
+            } else {
+                menu.findItem(R.id.action_edit_category).setVisible(false);
+            }
+            return true;
+        }
+
+        @Override
+        public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+            int id = item.getItemId();
+            switch(id) {
+                case R.id.action_edit_category:
+                    mode.finish();
+                    return true;
+                case R.id.action_delete_category:
+                    mode.finish();
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        @Override
+        public void onDestroyActionMode(ActionMode mode) {
+            mActionMode = null;
+            if (mAdapter.isChoiceMode()) {
+                mAdapter.clearSelection();
+            }
+            Activity activity = getActivity();
+            if (activity instanceof DrawerActivity) {
+                ((DrawerActivity) activity).unlockNavigationDrawer();
+            }
+        }
+    };
 
     public static CategoriesFragment newInstance(long parentCategoryId) {
         CategoriesFragment fragment = new CategoriesFragment();
@@ -47,6 +109,10 @@ public class CategoriesFragment extends Fragment implements LoaderManager.Loader
         args.putLong(ARG_PARENT_CATEGORY_ID, parentCategoryId);
         fragment.setArguments(args);
         return fragment;
+    }
+
+    public CategoriesFragment() {
+        // Required empty public constructor
     }
 
     @Override
@@ -62,35 +128,79 @@ public class CategoriesFragment extends Fragment implements LoaderManager.Loader
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_categories, container, false);
 
-        mRecyclerView = (RecyclerView) rootView.findViewById(R.id.list_categories);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST));
-        mRecyclerView.setHasFixedSize(true);
+        RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.list_categories);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL_LIST));
+        recyclerView.setHasFixedSize(true);
 
         mEmptyView = (TextView) rootView.findViewById(R.id.list_categories_empty);
         if (mParentCategoryId > 0) {
             mEmptyView.setText(getString(R.string.no_subcategories_defined));
         }
-        CategoriesAdapter adapter = new CategoriesAdapter(getActivity(), new CategoriesAdapter.OnClickListener() {
-                @Override
-                public void onClick(long categoryId, CategoriesAdapter.ViewHolder vh) {
-                    if (mParentCategoryId > 0) {
-                        return;
+
+        mAdapter = new CategoriesAdapter(getActivity());
+        recyclerView.setAdapter(mAdapter);
+
+        ItemClickHelper itemClickHelper = new ItemClickHelper(recyclerView);
+        itemClickHelper.setOnItemClickListener(new ItemClickHelper.OnItemClickListener() {
+            @Override
+            public void onItemClick(RecyclerView parent, View view, int position) {
+                boolean isItemSelected = mAdapter.isItemSelected(position);
+                if (isItemSelected) {
+                    mAdapter.setItemSelected(position, false);
+                    if (!mAdapter.isChoiceMode()) {
+                        mActionMode.finish();
+                    } else {
+                        mActionMode.invalidate();
                     }
+                } else if (mAdapter.isChoiceMode()) {
+                    mAdapter.setItemSelected(position, true);
+                    mActionMode.invalidate();
+                } else if (mParentCategoryId == 0) {
+                    long categoryId = parent.getAdapter().getItemId(position);
                     Intent subcategoryIntent = new Intent(getActivity(), SubcategoriesActivity.class);
                     subcategoryIntent.putExtra(SubcategoriesActivity.ARG_PARENT_CATEGORY_ID, categoryId);
                     getActivity().startActivity(subcategoryIntent);
                 }
-            });
-        mRecyclerView.setAdapter(adapter);
+            }
+        });
+        itemClickHelper.setOnItemLongClickListener(new ItemClickHelper.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(RecyclerView parent, View view, int position) {
+                mAdapter.setItemSelected(position, !mAdapter.isItemSelected(position));
+                if (mAdapter.isChoiceMode()) {
+                    if (mActionMode == null) {
+                        mActionMode = ((AppCompatActivity) getActivity()).startSupportActionMode(mActionModeCallback);
+                    } else {
+                        mActionMode.invalidate();
+                    }
+                } else {
+                    if (mActionMode != null) {
+                        mActionMode.finish();
+                    }
+                }
+                return true;
+            }
+        });
 
         return rootView;
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            mAdapter.onRestoreInstanceState(savedInstanceState);
+            if (mAdapter.isChoiceMode() && mActionMode == null) {
+                mActionMode = ((AppCompatActivity) getActivity()).startSupportActionMode(mActionModeCallback);
+            }
+        }
         getLoaderManager().initLoader(0, null, this);
         super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        mAdapter.onSaveInstanceState(outState);
     }
 
     @Override
@@ -119,12 +229,12 @@ public class CategoriesFragment extends Fragment implements LoaderManager.Loader
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        ((CategoriesAdapter) mRecyclerView.getAdapter()).swapCursor(data);
+        mAdapter.swapCursor(data);
         mEmptyView.setVisibility(data.getCount() > 0 ? View.GONE : View.VISIBLE);
     }
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-        ((CategoriesAdapter) mRecyclerView.getAdapter()).swapCursor(null);
+        mAdapter.swapCursor(null);
     }
 }
